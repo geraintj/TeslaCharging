d513356a628c1cd086ce715da4c65a4ef8eeed7b
Geraint Jones <geraint@geraintjones.com>
2020-08-01 17:13:00 +0100

    Fixed function endpoint access code with files

--- TeslaCharging.Web/teslachargingweb/src/environments/environment.prod.ts
+++ TeslaCharging.Web/teslachargingweb/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://teslacharging.azurewebsites.net/api',
-  apiCode: 'XVQaB8lo129AaWoBZpuUQUK45sl1JRyTkJqC1'
+  apiUrl: 'https://teslacharging.azurewebsites.net/api/charges/',
+  apiCode: 'XVQaB8lo129AaWoBZpuUQUK45sl1JRyTkJqC1/aZOuvS8CJ6f0WSgg=='
 };
