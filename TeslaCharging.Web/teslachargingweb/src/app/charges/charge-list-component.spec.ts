b6482a098df9518b924904ae40bacc9a2e8655d4
Geraint Jones <geraint@geraintjones.com>
2021-01-31 18:38:17 +0000

    Upgraded to Angular 11.x

--- TeslaCharging.Web/teslachargingweb/src/app/charges/charge-list-component.spec.ts
+++ TeslaCharging.Web/teslachargingweb/src/app/charges/charge-list-component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ChargeListComponentComponent } from './charge-list.component';
 
@@ -6,7 +6,7 @@ describe('ChargeListComponentComponent', () => {
   let component: ChargeListComponentComponent;
   let fixture: ComponentFixture<ChargeListComponentComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ChargeListComponentComponent ]
     })
